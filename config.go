bdcc5008cc4627c899adc358a642e542a4b964da
Shane Riddell <shane.riddell@ellucian.com>
2022-09-19 20:50:50 +0000

    Implement caching with sync once

--- config.go
+++ config.go
@@ -2,22 +2,48 @@ package awsutils
 
 import (
 	"context"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-func GetConfig(endpoint string, region string) (aws.Config, error) {
-	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		return aws.Endpoint{
-			PartitionID:   "aws",
-			URL:           endpoint,
-			SigningRegion: region,
-		}, nil
-	})
-	cfg, err := config.LoadDefaultConfig(
-		context.TODO(),
-		config.WithRegion(region),
-		config.WithEndpointResolverWithOptions(customResolver))
-	return cfg, err
+var once sync.Once
+var cachedConfig *aws.Config
+var cachedError error
+
+func GetConfig(region *string, endpoint *string) (*aws.Config, error) {
+	once.Do(func() { initConfig(region, endpoint) })
+
+	return cachedConfig, cachedError
+}
+
+func initConfig(region *string, endpoint *string) {
+	if endpoint == nil {
+		config, err := config.LoadDefaultConfig(
+			context.TODO(),
+			config.WithRegion(*region))
+		if err != nil {
+			cachedError = err
+		} else {
+			cachedConfig = &config
+		}
+	} else {
+		customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				PartitionID:   "aws",
+				URL:           *endpoint,
+				SigningRegion: region,
+			}, nil
+		})
+		config, err := config.LoadDefaultConfig(
+			context.TODO(),
+			config.WithRegion(*region),
+			config.WithEndpointResolverWithOptions(customResolver))
+		if err != nil {
+			cachedError = err
+		} else {
+			cachedConfig = &config
+		}
+	}
 }
